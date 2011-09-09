ce3a805ab3432d850fcf53ec8f7f0837eac07dec
Andy Shapiro <shapiromatron@gmail.com>
2011-09-08 23:34:13 -0400

    Added new custom extract commands for 7zip; specifically for extracting Excel images

--- Batch/Zip.bat
+++ Batch/Zip.bat
@@ -1,5 +1,6 @@
 :: Zip Commands
 :: http://www.codejacked.com/zip-up-files-from-the-command-line/
+:: http://www.dotnetperls.com/7-zip-examples
 
 :: ADD FILE TO ZIP FILE
 ::------------------------------------------
@@ -9,3 +10,9 @@
 ::------------------------------------------
 "C:\Program Files\7-zip\7z.exe" u -tzip text.zip outfile.txt && del outfile.txt
 
+:: Extract a specific file from a zip file
+::------------------------------------------
+:: If root directory:
+"C:\Program Files\7-zip\7z.exe" e TestZipFile.zip TestZipFile\outfile.txt
+:: If deeper directory (in this case, an Excel file):
+"C:\Program Files\7-zip\7z.exe" e Test.xlsx xl\media\image1.gif
